2b8e18c526d7d5058baaa98491337f05a7b04c4a
fajriyahdesianadwilestari <fajriyahdesi86@gmail.com>
2021-10-05 21:17:17 +0700

    Tugas Pertemuan2

--- pustaka-booking2/application/views/View_latian1.php
+++ pustaka-booking2/application/views/View_latian1.php
@@ -1,12 +1,14 @@
-<! DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
-      <title>Latihan 1</title>
+    <tittle>Latihan1</tittle>
 </head>
 <body>
-       Halo Kawan.. Yuk kita belajar web programming..!!!<br>
-       Nilai 1 = <?= $nilai1; ?>
-       Nilai 2 = <?= $nilai2; ?>
-       ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    Halo Kawan.. Yuk kita belajar web programming..!!!
+<br>
+    Nilai 1 =<?=$nilai1;?>
+    Nilai 2 =<?=$nilai2;?>
+    ini hasil dari pemodelan dengan menthod penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body>
 </html>
